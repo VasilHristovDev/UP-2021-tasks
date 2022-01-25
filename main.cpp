75517dc82dace871b8f457042407330efe3654cb
Vasil Hristov <vasil.hristov@netea.bg>
2022-01-25 21:54:39 +0200

    added a task for a formal language

--- main.cpp
+++ main.cpp
@@ -3,129 +3,59 @@
 
 const int MAX_SIZE_SENTENCE = 100;
 
-int countWordsInSentence(const char * sentence)
-{
-    int i = 0;
-    int counter = 0;
-    while(sentence[i])
-    {
-        if (sentence[i] == ' ')
-            counter++;
-        i++;
-    }
-    return counter + 1;
-}
-char * getLongestWord(char sentence[], int n)
-{
-    char ** wordsInSentence = new char * [countWordsInSentence(sentence)];
-    int i = 0;
-    int occupiedCharacters = 0;
-    int occupiedWords = 0;
-
-    wordsInSentence[occupiedWords] = new char [MAX_SIZE_SENTENCE];
-
-    while(sentence[i])
-    {
-        wordsInSentence[occupiedWords][occupiedCharacters++] = sentence[i];
-        if(sentence[i + 1 ] == ' ')
-        {
-            wordsInSentence[occupiedWords][occupiedCharacters] = '\0';
-            occupiedWords++;
-            wordsInSentence[occupiedWords] = new char [MAX_SIZE_SENTENCE];
-            occupiedCharacters = 0;
-            i++;
-        }
-        i++;
-    }
-
-    int maxCount = 0;
-    int jMax = 0;
-    for (int j = 0; j < occupiedWords ; ++j) {
-        if(maxCount < strlen(wordsInSentence[j]))
-        {
-            maxCount = strlen(wordsInSentence[j]);
-            jMax = j;
-        }
-    }
-    for (int j = 0; j < countWordsInSentence(sentence); ++j) {
-        if(j != jMax)
-            delete [] wordsInSentence[j];
-    }
-    return wordsInSentence[jMax];
-}
-
-int main1()
-{
-    int n;
-    char sentence[MAX_SIZE_SENTENCE];
-    std::cout<<"Enter n:";
-    std::cin>>n;
-    std::cin.ignore();
-    std::cin.getline(sentence,n);
-    sentence[n] = '\0';
-    char * ptrToLongestWord = getLongestWord(sentence,n);
-
-    std::cout<<ptrToLongestWord<<std::endl;
-    delete [] ptrToLongestWord;
-    return 0;
-}
-char * reverse (char * word)
-{
-    char * reversedWord = new char [strlen(word)];
-
-    for (int i = 0; i < strlen(word); ++i) {
-        reversedWord[i] = word[strlen(word) - i - 1];
-    }
-    reversedWord[strlen(word)] = '\0';
-    return reversedWord;
-}
-int timesWordIsSeenInSentence(const char * sentence, char * reversedWord)
-{
-    int i = 0;
-    int counterWord = 0;
-    int counterSameCharacters = 0;
-    int timesWordIsSeen = 0 ;
-    while(sentence[i])
-    {
-        for (int j = i; j < i + strlen(reversedWord); ++j) {
-            if(sentence[j] == reversedWord[counterWord++])
-            {
-                counterSameCharacters++;
-            }
-        }
-        if(counterSameCharacters == strlen(reversedWord))
-        {
-            timesWordIsSeen++;
-        }
-        counterSameCharacters = 0;
-        counterWord = 0;
-        i++;
-    }
-    return timesWordIsSeen;
-}
-
 int main()
 {
-    int n,m;
-    char sentence[MAX_SIZE_SENTENCE];
     char word[MAX_SIZE_SENTENCE];
-    std::cout<<"Enter size of sentence and size of word:";
-    std::cin>>n>>m;
-
-    std::cout<<"Enter word:";
     std::cin>>word;
 
-    std::cin.ignore();
-    std::cout<<"Enter sentence:";
-    std::cin.getline(sentence,n);
-    std::cout<<std::endl;
-    if(timesWordIsSeenInSentence(sentence, reverse(word)) > 0)
+    unsigned length = strlen(word);
+    int currentCharacter = 0;
+
+    char encodedWord[MAX_SIZE_SENTENCE];
+    int countChars = 0;
+
+    while(word[currentCharacter])
     {
-        std::cout<<"Word is seen "<<timesWordIsSeenInSentence(sentence, reverse(word))<<"times"<<std::endl;
+        if(word[currentCharacter] != 'a' && word[currentCharacter] != 'b' && word[currentCharacter] != 'c')
+        {
+            std::cerr<<"The word must contain only a, b and c!"<<std::endl;
+            return -1;
+        }
+        currentCharacter++;
     }
-    else
+    currentCharacter = 0;
+    int count = 0;
+    while (word[currentCharacter])
     {
-        std::cout<<"Word is not seen in sentence!"<<std::endl;
+        encodedWord[countChars++] = word[currentCharacter];
+        count = 0;
+        while(word[currentCharacter] == encodedWord[countChars - 1])
+        {
+            count++;
+            currentCharacter++;
+        }
+        encodedWord[countChars++] = count + '0';
     }
+    encodedWord[countChars] = '\0';
+    bool error = false;
+
+    if(encodedWord[0] != 'a' && encodedWord[0] != 'b')
+        error = true;
+    if(encodedWord[countChars - 2] != 'c' && encodedWord[countChars - 2] != 'b')
+        error = true;
+
+    int tempIterator = 0;
+    int tempCounter = 0;
+    while(encodedWord[tempIterator])
+    {
+        if(encodedWord[tempIterator] == 'a' || encodedWord[tempIterator] == 'b' || encodedWord[tempIterator] == 'c')
+        {
+            tempCounter++;
+        }
+        tempIterator++;
+    }
+    if(tempCounter > 3)
+        error = true;
+    std::cout<<(!error ? "It is correct" : "It is not correct")<<std::endl;
     return 0;
 }
